dd03789126a69eacacc2adbd6de00b6bd1047e29
chakib <celhouiti@gmail.com>
2019-01-29 22:17:03 +0100

    ajout de la classe TabString

---
+++ src/tabstring.cpp
@@ -0,0 +1,72 @@
+#include"tabstring.h"
+#include<iostream>
+using namespace std;
+    TabString::TabString():table(NULL),size(0)
+    {
+
+    }
+    TabString::TabString(ifstream &file):table(NULL),size(0)
+    {
+
+    }
+    TabString::TabString(const TabString &tab)
+    {
+        size = tab.size;
+        /*on copie les elements*/
+        for (int i = 0 ; i<size ; ++i)
+        {
+            table[i]=tab[i];
+        }
+    }
+    ~TabString::TabString()
+    {
+        if(table != NULL) delete[] table;
+    }
+    TabString &TabString::operator=(const TabString &tab)
+    {
+        if(this!=&tab)
+        {
+            size=tab.size;
+            /*on supprime l'ancien tableau*/
+            delete[] table;
+            /*on copie les elements*/
+            for(int i = 0 ; i < size ; ++i)
+            {
+                table[i]=tab[i]; 
+            }
+        }
+    }
+    void add(string str)
+    {
+        size++;
+        /*on cree un nouvel espace mémoire*/
+        string *copie= new string[size];
+        /*on copie les elements*/
+            for(int i = 0 ; i < size-1 ; ++i)
+            {
+                copie[i]=table[i]; 
+            }
+        /*on ajoute l'element à la dernière case*/
+        copie[size-1]=str;
+        /*suppression de l'ancien espace*/
+        delete[] table;
+        table=copie;
+
+    }
+    unsigned int TabString::get(string str) const
+    {
+        unsigned int i=0;
+        while(i<size())
+        {
+            if(table[i]==str) return i;
+            i++;
+        }
+    } 
+    string TabString::get(unsigned int i) const
+    {
+        return table[i];
+    }
+    unsigned int TabString::size()const
+    {
+        return size;
+    }
---
+++ src/tabstring.h
@@ -0,0 +1,22 @@
+#ifndef TABSTRING_H_
+#define TABSTRING_H_
+#include<string>
+#include<fstream>
+class TabString
+{
+    private:
+    std::string *table; 
+    unsigned int size; 
+    public:
+    TabString();
+    TabString(std::ifstream &file); 
+    TabString(const TabString &tab);
+    ~TabString();
+    TabString &operator=(const TabString &tab);
+    void add(std::string str);
+    unsigned int get(std::string str) const ;
+    std::string get(unsigned int i) const;
+    unsigned int size()const;
+};
+
+#endif
