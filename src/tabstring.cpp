7f73c793ec88c132f63bc7d46d14e83bd20c5891
Massili Kezzoul <massy.kezzoul@gmail.com>
2019-01-30 15:32:16 +0100

    Peite correction de la classe TabString

--- src/tabstring.cpp
+++ src/tabstring.cpp
@@ -1,72 +1,74 @@
-#include"tabstring.h"
-#include<iostream>
+#include <iostream>
+#include "tabstring.h"
+
 using namespace std;
-    TabString::TabString():table(NULL),size(0)
-    {
 
-    }
-    TabString::TabString(ifstream &file):table(NULL),size(0)
-    {
+TabString::TabString():table(NULL),size(0)
+{
 
-    }
-    TabString::TabString(const TabString &tab)
+}
+TabString::TabString(ifstream &file):table(NULL),size(0)
+{
+
+}
+TabString::TabString(const TabString &tab)
+{
+    size = tab.size;
+    /*on copie les elements*/
+    for (int i = 0 ; i<size ; ++i)
     {
-        size = tab.size;
-        /*on copie les elements*/
-        for (int i = 0 ; i<size ; ++i)
-        {
-            table[i]=tab[i];
-        }
+        table[i]=tab[i];
     }
-    ~TabString::TabString()
+}
+~TabString::TabString()
+{
+    if(table != NULL) delete[] table;
+}
+TabString &TabString::operator=(const TabString &tab)
+{
+    if(this!=&tab)
     {
-        if(table != NULL) delete[] table;
-    }
-    TabString &TabString::operator=(const TabString &tab)
-    {
-        if(this!=&tab)
-        {
-            size=tab.size;
-            /*on supprime l'ancien tableau*/
-            delete[] table;
-            /*on copie les elements*/
-            for(int i = 0 ; i < size ; ++i)
-            {
-                table[i]=tab[i]; 
-            }
-        }
-    }
-    void add(string str)
-    {
-        size++;
-        /*on cree un nouvel espace mémoire*/
-        string *copie= new string[size];
-        /*on copie les elements*/
-            for(int i = 0 ; i < size-1 ; ++i)
-            {
-                copie[i]=table[i]; 
-            }
-        /*on ajoute l'element à la dernière case*/
-        copie[size-1]=str;
-        /*suppression de l'ancien espace*/
+        size=tab.size;
+        /*on supprime l'ancien tableau*/
         delete[] table;
-        table=copie;
-
-    }
-    unsigned int TabString::get(string str) const
-    {
-        unsigned int i=0;
-        while(i<size())
+        /*on copie les elements*/
+        for(int i = 0 ; i < size ; ++i)
         {
-            if(table[i]==str) return i;
-            i++;
+            table[i]=tab[i]; 
         }
-    } 
-    string TabString::get(unsigned int i) const
-    {
-        return table[i];
     }
-    unsigned int TabString::size()const
+}
+void add(string str)
+{
+    size++;
+    /*on cree un nouvel espace mémoire*/
+    string *copie= new string[size];
+    /*on copie les elements*/
+        for(int i = 0 ; i < size-1 ; ++i)
+        {
+            copie[i]=table[i]; 
+        }
+    /*on ajoute l'element à la dernière case*/
+    copie[size-1]=str;
+    /*suppression de l'ancien espace*/
+    delete[] table;
+    table=copie;
+
+}
+unsigned int TabString::get(string str) const
+{
+    unsigned int i=0;
+    while(i<size())
     {
-        return size;
+        if(table[i]==str) return i;
+        i++;
     }
+} 
+string TabString::get(unsigned int i) const
+{
+    return table[i];
+}
+unsigned int TabString::size()const
+{
+    return size;
+}
--- src/tabstring.h
+++ src/tabstring.h
@@ -1,22 +1,26 @@
 #ifndef TABSTRING_H_
 #define TABSTRING_H_
-#include<string>
-#include<fstream>
-class TabString
-{
-    private:
+
+#include <string>
+#include <fstream>
+
+class TabString {
+private:
     std::string *table; 
     unsigned int size; 
-    public:
+
+public:
     TabString();
     TabString(std::ifstream &file); 
     TabString(const TabString &tab);
     ~TabString();
+
     TabString &operator=(const TabString &tab);
+
     void add(std::string str);
     unsigned int get(std::string str) const ;
     std::string get(unsigned int i) const;
-    unsigned int size()const;
+    unsigned int size() const;
 };
 
 #endif
