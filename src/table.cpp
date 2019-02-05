055b232f7e56b2d887a3c038a1250e880c345fea
chakib <celhouiti@gmail.com>
2019-02-05 21:35:53 +0100

    ajout de la méthode print

--- src/table.cpp
+++ src/table.cpp
@@ -2,6 +2,7 @@
 #include "table.h"
 #include<fstream>
 #include<cstdlib>
+#include <unistd.h>
 using namespace std; 
 
 
@@ -25,14 +26,52 @@ using namespace std;
     }
     void Table::print()
     {
-        /* Affichages des nom des attributs*/            
-        for(size_t i = 0; i < nomAttributs.getSize(); i++)
-            cout << nomAttributs.get(i) << " | ";
-        cout << endl << endl;
+        size_t *tabMax= new size_t[nomAttributs.getSize()];
+        size_t max,somme=0;
+        for(size_t i= 0 ; i < nomAttributs.getSize(); ++i)
+        {
+            max = nomAttributs.get(i).size();
+            for(size_t j=0 ; j < valeurAttributs.getSize();++j)
+            {
+                max = (valeurAttributs.get(j).get(i).size() < max ? max : valeurAttributs.get(j).get(i).size()); 
+            }
+            tabMax[i]=max;
+            somme+=tabMax[i];
+        }
+        cout << somme;
+
+        for(size_t i = 0; i < nomAttributs.getSize(); i++){
+            cout << tabMax[i] << " " ;
+        }
+        cout << endl;
+        //usleep(500000);
+        
+    
+        /* Affichages des nom des attributs*/
+        cout << "| ";        
+        for(size_t i = 0; i < nomAttributs.getSize(); i++){
+            cout << nomAttributs.get(i) ;
+            for (size_t k =0 ; k < tabMax[i]-nomAttributs.get(i).size() + 1;++k) {
+                cout<< " ";
+            }
+            cout << " | " ;
+        }
+        cout << endl;
+        /* affichage du séparateur */
+        for(size_t i = 0; i < somme+nomAttributs.getSize()*4+1; i++) {
+            cout << "-";
+        }
+        cout << endl;
         /* Affichages des valeurs des attributs */
         for(size_t i = 0; i < valeurAttributs.getSize(); i++) {
-            for(size_t j = 0; j < valeurAttributs.get(i).getSize() ; j++)
-                cout << valeurAttributs.get(i).get(j) << " | ";
+            cout << "| ";
+            for(size_t j = 0; j < valeurAttributs.get(i).getSize() ; j++){
+                cout << valeurAttributs.get(i).get(j);
+                for (size_t k =0 ; k < tabMax[j]-valeurAttributs.get(i).get(j).size()+1;++k){
+                    cout << " ";
+                }
+                cout << " | ";
+            }
             cout << endl;
         } 
     }
