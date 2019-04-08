8665d7fa59d1ce7692e747af199adc2dbef0c28c
chakib <celhouiti@gmail.com>
2019-04-08 12:08:53 +0200

    Debut de la classe TabAttribut

---
+++ src/tabAttribut.cpp
@@ -0,0 +1,78 @@
+#include "tabAttribut.h"
+#include "tabstring.h"
+
+
+
+    TabAttribut::TabAttribut():table(NULL),size(0),alloc(0){
+
+    }
+    TabAttribut::TabAttribut(string nomTable,string line):table(NULL),size(0),alloc(0)
+    {
+        strng *tab = NULL;
+        
+        size = TabString::strsplit2(line,tab,',');
+        if (size == 0) {
+            cerr << "Erreur dans Strsplit (size == 0)" << endl;
+            exit(1);
+        }
+        alloc = size;
+        table = new NomAttribut[alloc]; 
+        for(size_t i = 0; i < size; i++)
+        {
+            table[i]=nomAtrribut(nomTable,tab[i]);
+        }
+        
+    } 
+    TabAttribut::TabAttribut(unsigned long int):table(new NomAttribut[i]),size(0),alloc(0){
+    }// allouer 
+    TabAttribut::TabAttribut(NomAttribut*& str,unsigned long int taille):table(new NomAttribut[taille]),size(taille),alloc(taille){
+       int j=0;
+        for(size_t i = 0; i < taille; i++)
+        {
+          j=str[i].find_first_of(".");
+            if (j>taille)
+                table[i] = NomAttribut("",str[i]);
+            else {
+                table[i] = NomAttribut(str[i].substr(0,j),str[i].substr(j+1));
+            }   
+        }
+        
+    }
+    TabAttribut::TabAttribut(const TabAttribut &tab){
+
+    }
+    TabAttribut::TabAttribut(const TabAttribut&,const TabAttribut&){
+
+    } // projection 
+    TabAttribut::~TabAttribut(){
+
+    }
+
+    TabAttribut &TabAttribut::operator=(const TabAttribut &tab){
+
+    }
+    TabAttribut &TabAttribut::operator+=(const TabAttribut &tab){
+
+    }
+    NomAttribut &TabAttribut::operator[](unsigned long int){
+
+    }
+    const NomAttribut &TabAttribut::operator[](unsigned long int) const{
+
+    }
+
+
+    void TabAttribut::add(const NomAttribut& str){
+
+    }
+
+    unsigned long int TabAttribut::get(NomAttribut str) const {
+
+    }
+    NomAttribut TabAttribut::get(unsigned long int i) const{
+
+    }
+    unsigned long int TabAttribut::getSize() const{
+
+    }
+    
---
+++ src/tabAttribut.h
@@ -0,0 +1,32 @@
+#ifndef TABATTRIBUT_H_
+#define TABATTRIBUT_H_
+
+#include "nomAttribut.h"
+#include <string>
+class TabAttribut {
+private:
+    NomAttribut *table; 
+    unsigned long int size; 
+    unsigned long int alloc;
+public:
+    TabAttribut();
+    TabAttribut(std::string,std::string); 
+    TabAttribut(unsigned long int);// allouer 
+    TabAttribut(NomAttribut*& str,unsigned long int i);
+    TabAttribut(const TabAttribut &tab);
+    TabAttribut(const TabAttribut&,const TabAttribut&); // projection 
+    ~TabAttribut();
+
+    TabAttribut &operator=(const TabAttribut &tab);
+    TabAttribut &operator+=(const TabAttribut &tab);
+    NomAttribut &operator[](unsigned long int);
+    const NomAttribut &operator[](unsigned long int) const;
+
+
+    void add(const NomAttribut& str);
+
+    unsigned long int get(NomAttribut str) const ;
+    NomAttribut get(unsigned long int i) const;
+    unsigned long int getSize() const;
+};
+#endif
